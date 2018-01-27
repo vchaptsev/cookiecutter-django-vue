1419d6680551a23f2ce58a078aab91f34252996b
Victor Chaptsev <chaptsev.ifmo@gmail.com>
2018-01-27 05:12:22 +0300

    eslint

---
+++ {{cookiecutter.project_slug}}/.eslintrc.js
@@ -0,0 +1,21 @@
+// https://eslint.org/docs/user-guide/configuring
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
