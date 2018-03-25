ef269cf0d8e2fb00eb4ad9ccc7ff8b7a59371262
Victor Chaptsev <chaptsev.ifmo@gmail.com>
2018-03-25 14:09:09 +0300

    refactor: .eslintrc

--- {{cookiecutter.project_slug}}/.eslintrc.js
+++ {{cookiecutter.project_slug}}/.eslintrc.js
@@ -1,19 +1,10 @@
 // https://eslint.org/docs/user-guide/configuring
 module.exports = {
   root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true
-  },
-  extends: [
-    'plugin:vue/recommended',
-    'standard'
-  ],
-  plugins: [
-    'vue'
-  ],
+  parserOptions: { parser: 'babel-eslint' },
+  env: { browser: true },
+  extends: ['plugin:vue/recommended'],
+  plugins: ['vue'],
   rules: {
     'generator-star-spacing': 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
